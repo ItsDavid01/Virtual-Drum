675191257a0a77cb985be12bc25fb075f8413cbc
David Meza Santiago <83080655+ItsDavid01@users.noreply.github.com>
2022-05-31 18:57:15 -0500

    Added better images for the drum buttons

--- App.py
+++ App.py
@@ -9,16 +9,22 @@ ps.playsound("HihatA.mp3")
 ps.playsound("HihatA.mp3")
 ps.playsound("HihatA.mp3")
 import tkinter as tk
-import soundcard as sc
-import soundfile as sf
+from PIL import Image
+from PIL import ImageTk
 
 import record as rd
 
 class Tambor:
-    def __init__(self, sonido, nombre, root, x, y):
+    def __init__(self, sonido, nombre, root, x, y, image):
         self.sonido = sonido
         self.nombre = nombre
-        self.boton = tk.Button(root, text=nombre, command=self.golpe, padx=x, pady=y, bg="#428a6e")
+        self.image = Image.open(image)
+        self.image = self.image.resize((x,y))
+        self.image = ImageTk.PhotoImage(self.image)
+        
+        
+        
+        self.boton = tk.Button(root, text=nombre, command=self.golpe, padx=x, pady=y, bg="white", image=self.image, relief =tk.FLAT)
 
     def golpe(self):
         print("sonido de", self.nombre)
@@ -41,22 +47,22 @@ grabar.exportb.grid(row=1, column=4)
 grabar.recordLabel.grid(row=2, column=0)
 
 
-Redo = Tambor("redoblante.mp3", "Redoblante", app, 50, 50)
+Redo = Tambor("redoblante.mp3", "Redoblante", app, 150, 150, "Redoblante.jpg")
 Redo.boton.grid(row=0, column=2)
 
-tom1 = Tambor("tom1.mp3", "Tom 1", app, 50, 50)
+tom1 = Tambor("tom1.mp3", "Tom 1", app, 150, 150, "tom1.jpg")
 tom1.boton.grid(row=0, column=0)
 
-Bombo = Tambor("Bombo.mp3", "Bombo", app, 50, 50)
+Bombo = Tambor("Bombo.mp3", "Bombo", app, 150, 150, "Bombo.jpg")
 Bombo.boton.grid(row=1, column=0)
 
-platillo = Tambor("Platillo.mp3", "Platillo", app, 50, 50)
+platillo = Tambor("Platillo.mp3", "Platillo", app, 150, 150, "Platillo.jpg")
 platillo.boton.grid(row=0, column=1)
 
-HihatB = Tambor("HihatB.mp3", "", app, 60, 50)
+HihatB = Tambor("HihatB.mp3", "", app, 150, 150, "Hihat.jpg")
 HihatB.boton.grid(row=1, column=1)
 
-HihatA = Tambor("HihatA.mp3", "Hi Hat", app, 25, 25)
+HihatA = Tambor("HihatA.mp3", "Hi Hat", app, 70, 70, "Hihat.jpg")
 HihatA.boton.grid(row=1, column=1)
 
 
