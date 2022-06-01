3134f9d8e78012b167b6a0bf8d9375895679c57f
David Meza Santiago <83080655+ItsDavid01@users.noreply.github.com>
2022-05-31 21:24:18 -0500

    Code commented

--- App.py
+++ App.py
@@ -9,32 +9,36 @@ from PIL import ImageTk
 
 import record as rd
 
-class Tambor:
+class Tambor: #clase tambor para definir los componentes de la bateria
     def __init__(self, sonido, nombre, root, x, y, image):
         self.sonido = sonido
         self.nombre = nombre
+        #aqui se define la imagen que va a tener el componente de la bateria, este se convierte a un objeto imagen reconocible por tkinter primero
         self.image = Image.open(image)
         self.image = self.image.resize((x,y))
         self.image = ImageTk.PhotoImage(self.image)
         
+        #Se define el boton que al presionarse va a llamar el método golpe()
         self.boton = tk.Button(root, text=nombre, command=self.golpe, padx=x, pady=y, bg="white", image=self.image, relief =tk.FLAT)
 
     def golpe(self):
         print("Sonido de", self.nombre)
-        ps.playsound(self.sonido, block=False)
+        ps.playsound(self.sonido, block=False) #con la ayuda de playsound se reproduce el sonido correspondiente que esta guardado dentro de la carpeta sonidos
 
         
-app = tk.Tk()
-app.title("Virtual Drum")
-app.geometry("650x400")
+app = tk.Tk() #se inicializa la ventana principal de tkinter
+app.title("Virtual Drum") #aqui se cambia el titulo de la ventana
+app.geometry("650x400") # y aqui se establecen las dimensiones (AltoxAncho) que va a tener la ventana cuando se ejecute el programa
 
+#a continuacion creamos una instancia de la clase grabacion para obtener los botones de grabar, reproducir, y exportar
+#asi mismo tambien les asignamos una posición dentro de la ventana con el metodo .grid()
 grabar = rd.Grabacion(app, 100, 100)
 grabar.boton.grid(row=1, column=2)
 grabar.playb.grid(row=1, column=3)
 grabar.exportb.grid(row=0, column=3)
 grabar.recordLabel.grid(row=2, column=0)
 
-
+#tambien creamos instancias de la clase Tambor para obtener los diferentes componentes de la bateria
 Redo = Tambor(r"Audios\redoblante.mp3", "Redoblante", app, 150, 150, r"Imagenes\Redoblante.png")
 Redo.boton.grid(row=1, column=0)
 
@@ -54,4 +58,4 @@ HihatA = Tambor(r"Audios\HihatA.mp3", "Hi Hat", app, 70, 70, r"Imagenes\Hihat.pn
 HihatA.boton.grid(row=0, column=0)
 
 
-app.mainloop()
+app.mainloop() #Esta linea de código marca el final de la ventana que el módulo tkinter nos ofrece
--- record.py
+++ record.py
@@ -8,7 +8,8 @@ from PIL import ImageTk
         
 class Grabacion():
     def __init__(self, root, x, y) -> None:
-
+        #Aqui se define la imagen que tendra cada boton, exportar, reproducir y grabar respectivamente
+        #se abren las imagenes y se modifica el tipo de objeto para que concuerde con los paramtros dentro de la inicializacion del boton de tkinter
         self.exportButton = Image.open(r"Imagenes\ExportButton.jpeg")
         self.exportButton = self.exportButton.resize((x,y))
         self.exportButton = ImageTk.PhotoImage(self.exportButton)
@@ -23,21 +24,23 @@ class Grabacion():
         self.recButton = ImageTk.PhotoImage(self.recButton)
 
 
-
+        #aqui definimos los atributos de la clase y los botones
         self.texto = "Grabar"
         self.boton = tk.Button(root, text=self.texto, command=self.Grabar, padx=x, pady=y, bg="#428a6e", image=self.recButton)
         self.playb = tk.Button(root, text="Play", command=self.play, padx=x, pady=y, bg="red", image=self.playButton)
         self.exportb = tk.Button(root, text="Export", command=self.export, padx=x, pady=y, bg="blue", image=self.exportButton)
         self.recordLabel = tk.Label(root, text = "No se está grabando", height = 10, width = 15)
-        self.fs = 44100
-        self.root = root
+        self.fs = 44100 #se define la frecuencia a la que se grabarán los audios
+        self.root = root #root en este caso hará referencia a la ventana de tkinter que tenemos corriendo en el programa
 
     def Grabar(self):
+        #para grabar primero le pediremos al usuario una duracion de grabación usando una simple caja de dialogo
         answer = simpledialog.askinteger("Duración", "Ingrese la duración de la grabación en segundos: ", parent=self.root, minvalue=0, maxvalue=100)
         if (answer != None):
-            duration = answer  # seconds
+            duration = answer  # la duracion especificada estará en segundos
+            #se hace uso del método rec() perteneciente al modulo sounddevice para grabar en segundo plano los sonidos que se realicen con la bateria
             self.record = sd.rec(int(duration * self.fs), samplerate=self.fs, channels=2, device=" Mezcla estéreo (Realtek HD Audio Stereo input)")
-            self.recordLabel.config(text = "Grabando")
+            self.recordLabel.config(text = "Grabando") #cuando se empiece a grabar un texto en la parte inferior del programa nos mostrara que esta grabando
         else:
             messagebox.showwarning("Error","Valor de duración no válido, por favor ingrese otro.")
 
@@ -45,12 +48,15 @@ class Grabacion():
 
     def play(self):
         sd.stop()
+        #una vez terminemos de grabar, lo que se grabó se guardara en un arreglo de NumPy y estara listo para reproducirse o exportarse
         sd.play(self.record, self.fs)
         self.recordLabel.config(text = "Grabación finalizada.")
 
     def export(self):
+        #en caso de que se desee exportar, le pediremos el nombre bajo el cual desea exportar el archivo al usuario y procederemos a guardarlo en la carpeta del proyecto
         name = simpledialog.askstring("Nombre de la grabación", "Por favor, ingrese el nombre de la grabación: ", parent=self.root)
         if (name != None):
+            #luego se convierte el arreglo de NumPy a un archivo de tipo WAV reproducible 
             file = sf.write(f"{name}.wav", self.record, self.fs)
             self.recordLabel.config(text = "Grabacion finalizada.")
         else:
